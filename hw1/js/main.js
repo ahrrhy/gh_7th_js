e41f1459ba0c4373757598468c7676d627b9c064
ahrrhy <ahrrhy@gmail.com>
2017-10-18 20:36:14 +0300

    second and third tasks first hw

---
+++ hw1/js/main.js
@@ -0,0 +1,102 @@
+//task number 2
+
+
+var someOblect = {
+  propp1: "abc",
+  propp2: "def",
+  propp3:{
+    a:1,
+    b:2,
+    c:{
+        e:'s'
+    }
+  }
+};
+function clone(obj) {
+    var clone1 = {};
+    if(obj.prototype !==undefined){
+        clone1.prototype = obj.prototype;
+    }
+    for (var property in obj){
+        clone1[property] = obj[property];
+    }
+    return clone1;
+}
+var newObj = clone(someOblect);
+console.log(someOblect);
+console.log(newObj);
+newObj.propp1="fed";
+console.log(newObj);
+
+
+//task number 3
+
+
+function arrGenerate(n) {
+    var newArr = [];
+    for (var i = 0; i < n; i++) {
+        newArr[i]= i;
+    }
+    return newArr;
+}
+var arrQueue = arrGenerate(100000);
+
+function objGeneratorQueue(n) {
+    var obj = {};
+    for (var i = 0; i < n; i++){
+        obj[i]=i;
+    }
+    return obj;
+}
+var objQueue = objGeneratorQueue(100000);
+
+
+function objGeneratorRand(n) {
+    var obj = {},
+        propName = 'a';
+    for(var i = 1; i < n; i++){
+        propName += i;
+        obj[propName] = i;
+    }
+    return obj;
+}
+var objRand = objGeneratorRand(10000);
+console.log(objRand);
+
+function benchmarkObj(obj) {
+    for (var prop in obj){
+        console.log(prop);
+    }
+}
+
+console.time('arrQueue');
+benchmarkObj(arrQueue);
+console.timeEnd('arrQueue');
+
+console.time('arrQueue');
+benchmarkObj(arrQueue);
+console.timeEnd('arrQueue');
+
+console.time('randObj');
+benchmarkObj(objRand);
+console.timeEnd('randObj');
+
+function benchmarkArr(obj) {
+    var len = Object.getOwnPropertyNames(obj).length;
+    for(var i = 0; i<len; i++){
+        console.log(obj[i]);
+    }
+}
+
+console.time('arrQueue');
+benchmarkArr(arrQueue);
+console.timeEnd('arrQueue');
+
+console.time('arrQueue');
+benchmarkArr(arrQueue);
+console.timeEnd('arrQueue');
+
+console.time('randObj');
+benchmarkArr(objRand);
+console.timeEnd('randObj');
+
