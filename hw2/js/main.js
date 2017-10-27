edfda23643a4e2a492fe2eda814c7864b70cae57
ahrrhy <ahrrhy@gmail.com>
2017-10-27 19:52:09 +0300

    edit user object create nosubmiting form v1.1

--- hw2/js/main.js
+++ hw2/js/main.js
@@ -9,68 +9,67 @@ document.addEventListener('DOMContentLoaded', function(){
         e.preventDefault();
 
 
+            // taking data from userForm and make UserBirthday
         function UserBirthday () {
         var birthYear = document.getElementById("birthYear").value,
             birthMonth = document.getElementById("birthMonth").value,
             birthDay = document.getElementById("birthDay").value;
             return new Date(birthYear, birthMonth, birthDay);
         }
-        
-    })
-    
-});
+        console.log(UserBirthday());
 
 
+        //Create the User constructor
 
-//console.log(UserBirthday());
+        function User (fullName, UserBirthday) {
+            User.count++;
+            this.id = User.count;
+            this.fullName = fullName;
+            this.birthday;
+
+            // this descriptor gets value from birthday and defines User's age
+              Object.defineProperty (this, "age", {
+                get: function () {
+                  var todayYear = new Date().getFullYear();
+                  return todayYear - this.birthday.getFullYear();
+                }
+              });
 
 
-//Create the User constructor
-
-function User (fullName, birthday) {
-    User.count++;
-    this.id = User.count;
-    this.fullName = fullName;
-    this.birthday;
-
-    // this descriptor gets value from birthday and defines User's age
-      Object.defineProperty (this, "age", {
-        get: function () {
-          var todayYear = new Date().getFullYear();
-          return todayYear - this.birthday.getFullYear();
+            // these will return the value methods
+            this.valueOf = function () { // Number Value
+              return this.id;
+            };
+            this.toString =function () { // String Value
+                return this.fullName;
+            };
+            this.toJSON = function () {
+                return [this.id, this.fullName, this.age, this.birthday];
+            }
         }
-      });
+        User.count = 0;
+            });
+            
+        });
 
 
-    // these will return the value methods
-    this.valueOf = function () { // Number Value
-      return this.id;
-    };
-    this.toString =function () { // String Value
-        return this.fullName;
-    };
-    this.toJSON = function () {
-        return [this.id, this.fullName, this.age, this.birthday];
-    }
-}
-User.count = 0;
 
 
     // DEBAGS
-var vasya = new User("Василий Попкин");
-//console.log(vasya);
-console.log(vasya + ' a');
+// var vasya = new User("Василий Попкин");
+// //console.log(vasya);
+// console.log(vasya + ' a');
 
-var igor = new User('Igor Kulev');
-//console.log(misha);
-console.log(igor + ' a');
-console.log(Number(igor));
-console.log(String(igor));
-console.log(JSON.stringify(igor));
+// var igor = new User('Igor Kulev');
+// //console.log(misha);
+// console.log(igor + ' a');
+// console.log(Number(igor));
+// console.log(String(igor));
+// console.log(JSON.stringify(igor));
 
-// trying Date;
-function showDate(a, b, c){
-	var birthDate = new Date(a, b, c);
-	console.log(birthDate);
-}
-showDate(1990,8,8);
+// // trying Date;
+// function showDate(a, b, c){
+// 	var birthDate = new Date(a, b, c);
+// 	console.log(birthDate);
+// }
+// showDate(1990,8,8);
