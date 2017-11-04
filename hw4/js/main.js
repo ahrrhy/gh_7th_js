852b9718a386a5600d117a4185dd3f411ef936e7
ahrrhy <ahrrhy@gmail.com>
2017-11-04 22:36:55 +0200

    start 4rth

---
+++ hw4/js/main.js
@@ -0,0 +1,13 @@
+//we need to create one constructor Animal with 4 different instances one of which will have two lower instances using
+//Animal <- Mouse
+// Animal <- Eagle
+// Animal <- Deer
+// Animal <- Human
+// Animal <- Human <- Hunter
+// Animal <- Human <- Aborigine
+
+function Animal(name) {
+    this.health = 0;
+    this.speed = 0;
+
+}
