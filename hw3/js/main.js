1bd5b72fa928663efeed054161004b054d99726b
ahrrhy <ahrrhy@gmail.com>
2017-11-13 18:13:39 +0200

    trying 3rd v1.3

--- hw3/js/main.js
+++ hw3/js/main.js
@@ -7,28 +7,59 @@ var testObj = {
     }
 };
 
-function makeLog (obj) {
-    for (var key in obj) (function (key) {
-        return function () {
-            Object.defineProperties.bind(obj, key, {
-                get : function () {
-                    console.log(key);
-                    return key;
-                },
-                set: function(value) {
-                    console.log(value);
+// function makeLog (obj) {
+//     for (let key in obj) (function (key) {
+//         return function () {
+//             Object.defineProperties(obj, key.bind(this), {
+//                 get : function () {
+//                     console.log(key);
+//                     return key;
+//                 },
+//                 set: function(value) {
+//                     console.log(value);
+//                 }
+//             });
+//             return obj;
+//         }
+//     })(key);
+
+
+
+// }
+// var test = makeLog(testObj);
+// test.first = 2;
+// console.log(test.third());
+// console.log(test.first);
+// console.log(testObj.first);
+// test.third();
+
+
+function inject(obj, beforeFn) {
+    for (let propName of Object.getOwnPropertyNames(obj)) {
+        let prop = obj[propName];
+        // if (Object.prototype.toString.call(prop) === '[object Function]') {
+            obj[propName] = (function(fnName) {
+                return function() {
+                    beforeFn.call(this, fnName, arguments);
+                    return prop.apply(this, arguments);
                 }
-            });
-            return obj;
-        }
-    })(key);
-
-
-
+            })(propName);
+        // }
+    }
 }
-var test = makeLog(testObj);
-test.first = 2;
-console.log(test.third());
-console.log(test.first);
-console.log(testObj.first);
-test.third();
+
+function logFnCall(name, args) {
+    let s = name + '(';
+    for (let i = 0; i < args.length; i++) {
+        if (i > 0)
+            s += ', ';
+        s += String(args[i]);
+    }
+    s += ')';
+    console.log(s);
+}
+
+inject(testObj, logFnCall);
+testObj.third();
+
+
