e539052eca53eadced34892122f3af73b7305aa3
ahrrhy <ahrrhy@gmail.com>
2017-11-16 14:55:22 +0200

    5th find next direction

--- hw5/js/main.js
+++ hw5/js/main.js
@@ -1,7 +1,6 @@
 // main variables i used
 
-let outPut = "",
-    empty = ".",
+let empty = ".",
     animal = '@';
 const brash = "*";
 
@@ -9,7 +8,7 @@ let matrix = [
     [empty, empty, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty, empty, empty, brash],
     [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, brash, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, animal, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty],
+    [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty],
     [empty, empty, empty, empty, brash, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, brash, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty, empty],
@@ -21,25 +20,27 @@ let matrix = [
     [empty, brash, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
     [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, brash, empty],
     [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, brash, empty, empty, empty, empty, brash, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
 ];
 // it logs matrix
-const length = matrix.length;
-for (let i = 0; i < length; i++) {
-    let l = matrix[i].length;
-    for (let j = 0; j < l; j++) {
-        outPut += matrix[i][j] + " ";
+function showMap (matrix) {
+    let outPut = "";
+    const length = matrix.length;
+    for (let i = 0; i < length; i++) {
+        let l = matrix[i].length;
+        for (let j = 0; j < l; j++) {
+            outPut += matrix[i][j] + " ";
+        }
+        outPut += "\n";
     }
-    outPut += "\n";
+    console.log(outPut);
+    return matrix;
 }
-console.log(outPut);
-
+showMap(matrix);
 // find animal coordinates
-
+matrix[3][4] = animal;
 let animalPos = getAnimalPos(matrix, animal),
-    animalPosX = animalPos[0],
-    animalPosY = animalPos[1];
+    animalPosX = animalPos[1],
+    animalPosY = animalPos[0];
 function getAnimalPos(arr, animal) {
     for (let i = 0; i < arr.length; i++) {
         let index = arr[i].indexOf(animal);
@@ -48,41 +49,49 @@ function getAnimalPos(arr, animal) {
         }
     }
 }
-    //debug
-console.log(animalPos);
-console.log(animalPosX);
-console.log(animalPosY);
-
-
-
-const rand = function getRand(){
-    return Math.random();
-};
-
-const chooseDirection = function () {
-    return (rand() < 0.25) ? 'right' : (0.5 > rand() > 0.25) ? 'left' : (0.5 < rand() < 0.75) ? 'up' : 'down';
-};
-
-function move (arr, old_index, new_index) {
-    while (old_index < 0) {
-        old_index += arr.length;
+function getNextPos() {
+    let direction = chooseDirection();
+    if (direction === 'up') {
+        animalPosY += 1;
     }
-    while (new_index < 0) {
-        new_index += arr.length;
+    if (direction === 'down') {
+        animalPosY -= 1;
     }
-    if (new_index >= arr.length) {
-        let k = new_index - arr.length;
-        while ((k--) + 1) {
-            this.push(undefined);
-        }
+    if (direction === 'left') {
+        animalPosX -= 1;
     }
-    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-    return arr; // for testing purposes
+    if (direction === 'right') {
+        animalPosX += 1;
+    }
+    return animalPos = [animalPosY, animalPosX];
 }
-let mat = [empty, empty, empty, empty, animal, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty];
-console.log(mat);
-let matAnimalPos = getAnimalPos(mat, animal),
-    matAnimalPosX = matAnimalPos[0];
-//console.log(matAnimalPosX);
-mat = move(mat, matAnimalPosX, (matAnimalPosX + 1));
-console.log(mat);
+console.log(animalPos);
+console.log(getNextPos());
+console.log(animalPosY, animalPosX);
+    //debug
+
+// console.log(animalPosX);
+// console.log(animalPosY);
+// console.log(animalPos);
+
+function animalMove(arr, animal) {
+    let currentPos = animalPos(arr, animal),
+        nextPos;
+}
+showMap(matrix);
+
+function rand(){
+    return Math.random();
+}
+
+function chooseDirection() {
+    return (rand() < 0.25) ? 'right' : (0.5 > rand() > 0.25) ? 'left' : (0.5 < rand() < 0.75) ? 'up' : 'down';
+}
+
+// let mat = [empty, empty, empty, empty, animal, empty, empty, empty, empty, empty, empty, brash, empty, empty, empty, empty];
+// console.log(mat);
+// let matAnimalPos = getAnimalPos(mat, animal),
+//     matAnimalPosX = matAnimalPos[0];
+// //console.log(matAnimalPosX);
+// mat = move(mat, matAnimalPosX, (matAnimalPosX - 5));
+// console.log(mat);
